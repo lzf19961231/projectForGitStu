e57099342d3b60eeb0e021a73440b73f957c17c4
lzf19961231 <lzf19961231@sina.com>
2023-05-09 11:45:39 +0800

    第一次提交

---
+++ src/main/java/com/example/projectforgitstu/ProjectForGitStuApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectforgitstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForGitStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectForGitStuApplication.class, args);
+    }
+
+}
